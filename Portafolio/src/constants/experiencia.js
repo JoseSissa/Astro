f70e1cb1f99bbaefb8ad6bcfb410a28d7e85cceb
Jose <sissamendoza@outlook.com>
2024-03-25 16:06:37 -0500

    Experiencia Sobre mi

--- Portafolio/src/constants/experiencia.js
+++ Portafolio/src/constants/experiencia.js
@@ -1,9 +1,9 @@
-export const EXPERIENCE_JOBS = {
-	work_one: {
+export const EXPERIENCE_JOBS = [
+	{
 		fecha: '2022 - Presente',
 		empresa: 'Gravital',
 		cargo: 'Desarrollador Frontend',
 		explicacion_cargo: 'Desarrollador Frontend que se enfocaba en trabajar en Novita Diamonds.',
 		tecnologias: ['HTML', 'CSS', 'JavaScript']
 	}
-}
+]
