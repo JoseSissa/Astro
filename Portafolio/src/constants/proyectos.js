bc82d524cf5d54f992ccfcab3d8fca584c300995
Jose <sissamendoza@outlook.com>
2024-03-25 17:56:00 -0500

    Proyectos

---
+++ Portafolio/src/constants/proyectos.js
@@ -0,0 +1,10 @@
+export const PROJECTS = [
+	{
+		title: '2022 - Presente',
+		description: 'Gravital',
+		image: 'Desarrollador Frontend',
+        image_src: 'Descripción de la imagen',
+		demo: 'Desarrollador Frontend que se enfocaba en trabajar en Novita Diamonds.',
+		code: 'JavaScript'
+	}
+]
