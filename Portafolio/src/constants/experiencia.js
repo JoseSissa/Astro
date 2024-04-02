1b40d9990a55c7b87daa8b59c4e75da0bc4e4aa8
Jose <sissamendoza@outlook.com>
2024-04-02 16:08:33 -0500

    Componente experiencia completado

--- Portafolio/src/constants/experiencia.js
+++ Portafolio/src/constants/experiencia.js
@@ -2,8 +2,33 @@ export const EXPERIENCE_JOBS = [
 	{
 		fecha: '2022 - Presente',
 		empresa: 'Gravital',
+		web: 'https://gravital.co/',
 		cargo: 'Desarrollador Frontend',
-		explicacion_cargo: 'Desarrollador Frontend que se enfocaba en trabajar en Novita Diamonds.',
-		tecnologias: ['HTML', 'CSS', 'JavaScript']
+		explicacion_cargo: 'Desarrollador Frontend donde una de mis principales funciones era agregar, mejorar y modificar diferentes funciones desde la parte del cliente a la e-commerce Novita Diamonds.',
+		tecnologias: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		fecha: '2022 - Presente',
+		empresa: 'Gravital',
+		web: 'https://gravital.co/',
+		cargo: 'Desarrollador Frontend',
+		explicacion_cargo: 'Desarrollador Frontend donde una de mis principales funciones era agregar, mejorar y modificar diferentes funciones desde la parte del cliente a la e-commerce Novita Diamonds.',
+		tecnologias: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		fecha: '2022 - Presente',
+		empresa: 'Gravital',
+		web: 'https://gravital.co/',
+		cargo: 'Desarrollador Frontend',
+		explicacion_cargo: 'Desarrollador Frontend donde una de mis principales funciones era agregar, mejorar y modificar diferentes funciones desde la parte del cliente a la e-commerce Novita Diamonds.',
+		tecnologias: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		fecha: '2022 - Presente',
+		empresa: 'Gravital',
+		web: 'https://gravital.co/',
+		cargo: 'Desarrollador Frontend',
+		explicacion_cargo: 'Desarrollador Frontend donde una de mis principales funciones era agregar, mejorar y modificar diferentes funciones desde la parte del cliente a la e-commerce Novita Diamonds.',
+		tecnologias: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
 	}
 ]
