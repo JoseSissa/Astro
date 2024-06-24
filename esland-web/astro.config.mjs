10f76868586dfa3f8d4f6550a63a9fc7c4303d15
Jose <sissamendoza@outlook.com>
2024-06-24 10:02:58 -0500

    Authentication with twtich

--- esland-web/astro.config.mjs
+++ esland-web/astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+import vercel from '@astrojs/vercel/serverless';
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), auth()],
+  output: 'server',
+  adapter: vercel(),
   i18n: {
     // Idioma por defecto
     defaultLocale: 'es',
